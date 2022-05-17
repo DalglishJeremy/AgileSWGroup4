20f38a46542369a0400067f03dc66125135a9558
Collin Wisser <36163206+Collin24910@users.noreply.github.com>
2022-05-17 17:17:14 -0400

    Created endpoint for NewsAPI

--- Sports/views.py
+++ Sports/views.py
@@ -1,14 +1,45 @@
+from sqlite3 import Time
+from time import time
 from django.shortcuts import render
+from newsapi import NewsApiClient
+import time
 
 def Home(request):
-	return render(request, 'Sports/home.html')
+    return render(request, 'Sports/home.html')
 
 def NewsHome(request):
-	return render(request, 'Sports/new-home.html')
+
+    currentDate = time.strftime("%Y-%m-%d")
+    newsapi = NewsApiClient(api_key='fbfba5ccce3947d7987f287a603127c0')
+
+    all_articles = newsapi.get_everything(q='football OR baseball OR golf OR basketball',
+                                      from_param='2022-05-16',
+                                      to=currentDate,
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page=2)
+
+    articles = all_articles['articles']
+    length = len(articles)
+    desc =[]
+    title =[]
+    img =[]
+    artURL = []
+    for i in range(length):
+        f = articles[i]
+        title.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+        artURL.append(f['url'])
+
+
+    mylist = zip(title, desc, img, artURL)
+    context = {'mylist': mylist}
+    return render(request, 'Sports/news-home.html', context)
 
 def SportsHome(request):
-	return render(request, 'Sports/sports-home.html')
+    return render(request, 'Sports/sports-home.html')
 
 def WeatherHome(request):
-	return render(request, 'Sports/weather-home.html')
+    return render(request, 'Sports/weather-home.html')
 # Create your views here.
