a1f15f66d4fd0269e84104a3d1c26078a41b2631
tlentz98 <len1131@comcast.net>
2022-05-22 10:11:32 -0400

    Weather App Part 1

--- Sports/models.py
+++ Sports/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.conf import settings
 
+class City(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self): #show the actual city name on the dashboard
+        return self.name
+    
+    class Meta: 
+        verbose_name_plural = 'cities'
+
 class MlbBoxScoreData(models.Model):
 	boxscore = models.TextField()
 	away_name = models.TextField()
 	away_abbr = models.TextField(max_length = 5)
 	away_score = models.IntegerField()
 	home_name = models.TextField()
-	home_abbr = models.TextField(max_length = 5)
-	home_score = models.IntegerField()
 	winning_name = models.TextField()
 	winning_abbr = models.TextField(max_length = 5)
 	losing_name = models.TextField()
--- Sports/views.py
+++ Sports/views.py
@@ -3,7 +3,44 @@ from time import time
 from django.shortcuts import render
 from .models import *
 from Sports.newsApi import getNews
+import requests
+from .models import City
+from .forms import CityForm
 
+def WeatherHome(request):
+    cities = City.objects.all()
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=7b59b05d3e410e80c65cd3a16e85f8e1'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    weather_data = []
+
+    for city in cities:
+        city_weather = requests.get(url.format(city)).json()
+
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon'],
+            'humidity': city_weather['main']['humidity'],
+            'pressure': city_weather['main']['pressure'],
+            'country': city_weather['sys']['country'],
+            'sunrise': city_weather['sys']['sunrise'],
+            'sunset': city_weather['sys']['sunset'],
+            'windspeed': city_weather['wind']['speed']
+        }
+
+        weather_data.append(weather)
+
+    context = {'weather_data': weather_data, 'form': form}
+
+    return render(request, 'Sports/weather-home.html', context)
 
 def Home(request):
     return render(request, 'Sports/home.html')
@@ -11,14 +48,14 @@ def Home(request):
 def NewsHome(request):
     input_value = 'null'
     context = getNews(input_value)
-    return render(request, 'Sports/news-home.html', context)
+    return render(request, 'Sports/news-home.html')
 
 def NewsHome_search(request):
     if request.method == 'GET':
         search_query = request.GET.get('search_box', None)
     
     context = getNews(search_query)
-    return render(request, 'Sports/news-home.html', context)
+    return render(request, 'Sports/news-home.html')
 
 def SportsHome(request):
     return render(request, 'Sports/sports-home.html')
@@ -30,8 +67,6 @@ def BaseballPage(request):
 				{'mlbGames': mlbGames})
 
 def BasketballPage(request):
-	return render(request, 'Sports/basketball.html')
+    return render(request, 'Sports/basketball.html')
 
-def WeatherHome(request):
-    return render(request, 'Sports/weather-home.html')
-# Create your views here.
+
