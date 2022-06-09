77e941a519ea7c482aa41c3c51a7821cd153ff2a
Mike <mczacierka@gmail.com>
2022-06-09 18:13:48 -0400

    WeatherAPI and tests

--- Sports/views.py
+++ Sports/views.py
@@ -8,46 +8,20 @@ import requests
 from .models import City
 from .forms import CityForm, BaseballDateForm
 import datetime
+from Sports.API.weatherAPI import fetchWeather
 
 def WeatherHome(request):
-    city = ""
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=7b59b05d3e410e80c65cd3a16e85f8e1'
-    #alerts_url='https://api.openweathermap.org/data/3.0/onecall?lat={}&lon={}&exclude=minutely,hourly,daily&appid=7b59b05d3e410e80c65cd3a16e85f8e1'
+    dat = {}
+    ctx = {'weather': dat, 'form': CityForm(), 'alert':{}}
     if request.method == 'POST':
-        form = CityForm(request.POST)
-        if form.is_valid():
-            city = form.cleaned_data['name']
+        ctx['form'] = CityForm(request.POST)
+        if ctx['form'].is_valid():
+            city = ctx['form'].cleaned_data['name']
+            weather = fetchWeather(city)
+            ctx['weather'] = weather['weather']
+            ctx['alerts'] = weather['alerts']
 
-    form = CityForm()
-    city_weather = {}
-    alerts = {}
-    city_req = requests.get(url.format(city))
-    if city_req.status_code == 200: # got valid city
-        city_weather = city_req.json()
-        weather = {
-            'city' : city,
-            'temperature' : city_weather['main']['temp'],
-            'description' : city_weather['weather'][0]['description'].capitalize(),
-            'icon' : "http://openweathermap.org/img/w/{}.png".format(city_weather['weather'][0]['icon']),
-            'humidity': city_weather['main']['humidity'],
-            'pressure': city_weather['main']['pressure'],
-            'country': city_weather['sys']['country'],
-            'sunrise': datetime.datetime.fromtimestamp(city_weather['sys']['sunrise']),
-            'sunset': datetime.datetime.fromtimestamp(city_weather['sys']['sunset']),
-            'windspeed': city_weather['wind']['speed']
-            }
-        # alert_req = requests.get(alerts_url.format(city_weather['coord']['lat'], city_weather['coord']['lon']))
-        # if alert_req == 200:
-        #     alert_res = alert_req.json()
-        #     alerts = {
-        #         'alerts': alert_res['alerts']
-        #     }
-    else:
-        weather = {}
-
-    context = {'weather': weather, 'form': form, 'alerts': alerts}
-
-    return render(request, 'Sports/weather-home.html', context)
+    return render(request, 'Sports/weather-home.html', ctx)
 
 
 def BaseballPage(request):
